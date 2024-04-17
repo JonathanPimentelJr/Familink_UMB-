e33e87931444dfb83a08463295916c576cf83f37
Jonathan Pimentel <jonathanpimenteljr@icloud.com>
2024-04-17 12:17:15 -0400

    missing info on the README file

--- README.md
+++ README.md
@@ -37,3 +37,7 @@ Use Version 2 of NativeWind (Using Tailwind CSS)
 npm install nativewind
 npm install --save-dev tailwindcss@3.3.2
 npx tailwindcss init 
+
+Run on the app:
+npx expo start 
+npx expo start -c is for without caching 
--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,43 +1,34 @@
 import { View, Text } from 'react-native'
 import {Tabs, Redirect} from 'expo-router'
-import {icons} from '../../constants';
+import {icons} from '../../constants'
 
-const TabIcon = ({icon, color, name, focused}) => {
+const TabIcon = ({icon,color, name, focused}) => {
     return (
         <View>
             <Image 
-            source= {icon}
-            resizeMode= "contain"
-            tintColor= {color}
-            className= "w-6 h-6"
+                source={icons} 
             />
         </View>
     )
 }
 
-
 const TabsLayout = () => {
-  return (
-    <>
-        <Tabs>
-            <Tabs.Screen 
-            name= "home" 
-            options = {{
-                title: 'Home',
-                headerShown: false,
-                tabBarIcon: ({color, focused }) => (
-                  <TabIcon 
-                    icon= {icons.home}
-                    color= {color}
-                    name= "Home"
-                    focused= {focused}
-                  />
-                )
-            }}
-          />
-        </Tabs>
-    </>
-  )
+    return (
+        <>
+            <Tabs>
+                <Tabs.Screen 
+                    name="home"
+                    options= {{
+                        title: "Home",
+                        headerShown: false
+                        
+                    }}
+                />
+            </Tabs>
+        </>
+        
+    )
 }
+
 
 export default TabsLayout
