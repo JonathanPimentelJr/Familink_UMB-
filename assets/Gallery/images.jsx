79bc9c8202231ac580d65462453f57ed13afe157
kalyanrammamidipaka <70712219+kalyanrammamidipaka@users.noreply.github.com>
2024-05-03 00:49:12 -0400

    final

---
+++ assets/Gallery/images.jsx
@@ -0,0 +1,14 @@
+
+export const imagePaths = [
+    require('./00001.jpg'),
+    require('./00002.jpg'),
+    require('./00003.jpg'),
+    require('./00004.jpg'),
+    require('./00005.jpg'),
+    require('./00006.jpg'),
+    require('./00007.jpg'),
+    require('./00008.jpg'),
+    require('./00009.jpg'),
+    require('./00010.jpg'),
+  ];
+  
